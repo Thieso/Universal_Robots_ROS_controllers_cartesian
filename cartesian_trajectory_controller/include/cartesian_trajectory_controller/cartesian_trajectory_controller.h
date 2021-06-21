831719d4e4ac8f0a8edbd3d188043025e4e07abe
Thies Oelerich <thies.oelerich@iwu.fraunhofer.de>
2021-06-21 08:26:11 +0200

    Added real time capabilities by added a service client which can read a sensor signal to the control loop

--- cartesian_trajectory_controller/include/cartesian_trajectory_controller/cartesian_trajectory_controller.h
+++ cartesian_trajectory_controller/include/cartesian_trajectory_controller/cartesian_trajectory_controller.h
@@ -1,19 +1,32 @@
-// -- BEGIN LICENSE BLOCK ----------------------------------------------
-// Copyright 2020 FZI Forschungszentrum Informatik
-// Created on behalf of Universal Robots A/S
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2020 FZI Research Center for Information Technology
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// -- END LICENSE BLOCK ------------------------------------------------
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////
 
 //-----------------------------------------------------------------------------
 /*!\file    cartesian_trajectory_controller.h
@@ -34,62 +47,68 @@
 #include <atomic>
 #include <mutex>
 #include <actionlib/server/simple_action_server.h>
-#include <speed_scaling_interface/speed_scaling_interface.h>
+#include <cartesian_interface/speed_scaling_interface.h>
 
 namespace cartesian_trajectory_controller
 {
-template <class HWInterface>
-class CartesianTrajectoryController : public ros_controllers_cartesian::ControlPolicy<HWInterface>
-{
-public:
-  CartesianTrajectoryController() : ros_controllers_cartesian::ControlPolicy<HWInterface>(){};
 
-  virtual ~CartesianTrajectoryController(){};
-
-  bool init(hardware_interface::RobotHW* hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh) override;
-
-  void starting(const ros::Time& time) override;
-
-  void stopping(const ros::Time& time) override;
-
-  void update(const ros::Time& time, const ros::Duration& period) override;
-
-  void executeCB(const cartesian_control_msgs::FollowCartesianTrajectoryGoalConstPtr& goal);
-
-  void preemptCB();
-
-protected:
-  using ControlPolicy = ros_controllers_cartesian::ControlPolicy<HWInterface>;
-
-  struct TrajectoryDuration
+  template <class HWInterface>
+    class CartesianTrajectoryController : public cartesian_ros_control::ControlPolicy<HWInterface>
   {
-    TrajectoryDuration() : now(0.0), end(0.0)
-    {
-    }
 
-    ros::Duration end;  ///< Planned target duration of the current action.
-    ros::Duration now;  ///< Current duration of the current action.
+    public:
+      CartesianTrajectoryController()
+        : cartesian_ros_control::ControlPolicy<HWInterface>()
+      {};
+
+      virtual ~CartesianTrajectoryController(){};
+
+      bool init(hardware_interface::RobotHW* hw,
+          ros::NodeHandle& root_nh,
+          ros::NodeHandle& controller_nh) override;
+
+      void starting(const ros::Time& time) override;
+
+      void stopping(const ros::Time& time) override;
+
+      void update(const ros::Time& time, const ros::Duration& period) override;
+
+      void executeCB(const cartesian_control_msgs::FollowCartesianTrajectoryGoalConstPtr& goal);
+
+      void preemptCB();
+
+    protected:
+      using ControlPolicy = cartesian_ros_control::ControlPolicy<HWInterface>;
+
+      struct TrajectoryDuration
+      {
+        TrajectoryDuration() : now(0.0), end(0.0) {}
+
+        ros::Duration end; ///< Planned target duration of the current action.
+        ros::Duration now; ///< Current duration of the current action.
+      };
+
+      void timesUp();
+
+      void monitorExecution(const cartesian_ros_control::CartesianState& error);
+
+      bool withinTolerances(const cartesian_ros_control::CartesianState& error,
+                            const cartesian_control_msgs::CartesianTolerance& tolerance);
+
+    private:
+      std::unique_ptr<hardware_interface::SpeedScalingHandle> speed_scaling_;
+      std::unique_ptr<actionlib::SimpleActionServer<cartesian_control_msgs::FollowCartesianTrajectoryAction> >
+        action_server_;
+      ros::ServiceClient dist_sensor_serv;
+      double offset;
+      std::atomic<bool> done_;
+      std::mutex lock_;
+      cartesian_ros_control::CartesianTrajectory trajectory_;
+      TrajectoryDuration trajectory_duration_;
+      cartesian_control_msgs::CartesianTolerance path_tolerances_;
+      cartesian_control_msgs::CartesianTolerance goal_tolerances_;
   };
 
-  void timesUp();
-
-  void monitorExecution(const ros_controllers_cartesian::CartesianState& error);
-
-  bool withinTolerances(const ros_controllers_cartesian::CartesianState& error,
-                        const cartesian_control_msgs::CartesianTolerance& tolerance);
-
-private:
-  std::unique_ptr<scaled_controllers::SpeedScalingHandle> speed_scaling_;
-  std::unique_ptr<actionlib::SimpleActionServer<cartesian_control_msgs::FollowCartesianTrajectoryAction> >
-      action_server_;
-  std::atomic<bool> done_;
-  std::mutex lock_;
-  ros_controllers_cartesian::CartesianTrajectory trajectory_;
-  TrajectoryDuration trajectory_duration_;
-  cartesian_control_msgs::CartesianTolerance path_tolerances_;
-  cartesian_control_msgs::CartesianTolerance goal_tolerances_;
-};
-
-}  // namespace cartesian_trajectory_controller
+}
 
 #include <cartesian_trajectory_controller/cartesian_trajectory_controller.hpp>
